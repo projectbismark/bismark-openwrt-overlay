efdcd528d060c27a0ef8f452b826f263239f21e3
Walter DeDonato <bismark@dp4.gtnoise.net>
2012-03-29 02:00:38 +0000

    mkdir before trying to move packages into them

--- scripts/special-packages.sh
+++ scripts/special-packages.sh
@@ -5,10 +5,11 @@ delim=" "
 base_dir=`pwd`
 build_dir="bin/ar71xx"
 dirs="packages updates experiments"
-awk '/^[[:alnum:]\-]+ [[:alnum:]\-]+[ ]*$/' $config_file | while read i; do
+awk '/^[[:alnum:]\-]+ [[:alnum:]\-]+[ ]*$/' $config_file | sort -u | while read i; do
 	package=`echo $i | cut -d"$delim" -f1`
 	dir=`echo $i | cut -d"$delim" -f2`	
 	mkdir -p "$build_dir"/"$dir"
+	touch "$build_dir"/"$dir"/Upgradable
 	echo Moving $package to $dir
 	mv -f "$base_dir"/"$build_dir"/packages/"$package"_*.ipk "$base_dir"/"$build_dir"/"$dir"
 done

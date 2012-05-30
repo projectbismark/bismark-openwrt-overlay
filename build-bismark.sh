9f6df112a08caa19afea86318c58ec21b288e183
Stephen Woodrow <srwoodrow@gmail.com>
2012-05-29 20:40:19 -0400

    Add build parameters to /etc/{banner,issue}.
    build-bismark now provides the following variables for replacement
    in parameterized files in the build root:
    * BISMARK_RELEASE: the release name as used in package repos, etc.
      e.g. s/BISMARK_RELEASE/quirm/
    * BISMARK_PRETTY_RELEASE: the release name, capitalized:
      e.g. s/BISMARK_PRETTY_RELEASE/Quirm/
    * BISMARK_BUILD_DATE: the date on which the build-bismark script was
      executed, in ISO-8601 format.
      e.g. s/BISMARK_BUILD_DATE/2012-05-29T23:59:59+0000/
    * BISMARK_HASH: the full git commit hash of the HEAD of
      bismark-openwrt-overlay
      e.g. s/BISMARK_HASH/cbd64445209f6e2f20d841df802a598a05a585d5/
    * BISMARK_SHORTHASH: the abbreviated git commit hash of the HEAD of
      bismark-openwrt-overlay
      e.g. s/BISMARK_SHORTHASH/cbd6444/
    
    Update opkg.conf parameters accordingly.

--- build-bismark.sh
+++ build-bismark.sh
@@ -8,19 +8,41 @@
 #
 # where BUILDROOT is something like "quirm-rc3"
 
+# Build parameters
+BISMARK_RELEASE="fourecks"
+WEB_ROOT=/data/users/bismark/builds
+
 set -o errexit
 set -o nounset
 
-WEB_ROOT=/data/users/bismark/builds
-RELEASE_NAME=$(sed -n '2p' files/etc/issue)
+
+# Computed build parameters
+BISMARK_PRETTY_RELEASE=$(echo $BISMARK_RELEASE | \
+        awk '{print toupper(substr($0,1,1)) substr($0,2)}')
+BISMARK_SHORTHASH=$(git log -n 1 --pretty=format:%h HEAD)
+BISMARK_HASH=$(git log -n 1 --pretty=format:%H HEAD)
+BISMARK_BUILD_DATE=$(date -u  --iso-8601=seconds)
 
 svn co svn://svn.openwrt.org/openwrt/tags/backfire_10.03.1 . --force
+param_files="files/etc/issue
+             files/etc/banner
+             files/etc/opkg.conf"
+
 ./scripts/feeds update
 git checkout -- .config
 ./scripts/feeds install -a
-git checkout -- files/etc/opkg.conf
-sed -i "s/BISMARK-RELEASE/$RELEASE_NAME/" files/etc/opkg.conf
+
+# Substitute parameters in each of param_files
+for file in $param_files; do
+    git checkout -- $file
+    sed -i "s/BISMARK_RELEASE/$BISMARK_RELEASE/g; \
+            s/BISMARK_PRETTY_RELEASE/$BISMARK_PRETTY_RELEASE/g; \
+            s/BISMARK_HASH/$BISMARK_HASH/g; \
+            s/BISMARK_SHORTHASH/$BISMARK_SHORTHASH/g; \
+            s/BISMARK_BUILD_DATE/$BISMARK_BUILD_DATE/g" $file
+done
+
 make -j 4
 
-WEB_DIR=$WEB_ROOT/$RELEASE_NAME
+WEB_DIR=$WEB_ROOT/$BISMARK_RELEASE
 scripts/release-to-www.sh $WEB_DIR

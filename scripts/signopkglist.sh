9e20e80b7550449c078c692a9bb2cd37f254d9df
gmartins <ggmartins@gmail.com>
2013-10-21 13:07:33 +0000

    signopkg fixes

--- scripts/signopkglist.sh
+++ scripts/signopkglist.sh
@@ -1,35 +1,38 @@
 #!/bin/bash
 
-O="/O=Georgia Tech Network Operations and Internet Security Lab/OU=Project Bismark"
-CN="/CN=downloads.projectbismark.net"
-EA="/emailAddress = ssl-admin@projectbismark.net"  
+O="O=Georgia Tech Network Operations and Internet Security Lab/OU=Project Bismark/"
+CN="CN=downloads.projectbismark.net/"
+EA="emailAddress=ssl-admin@projectbismark.net"  
+
+SSLDIR=ssl
 
 CERTNAME=opkgcert.pem
 PUBKEY=serverCA.pem
 
-#generate both pub & priv keys in PEM format
-openssl req -x509 -nodes -days 1800 \
-  -subj "/C=US/ST=Georgia/L=Atlanta/$O$CN$EA" \
-  -newkey rsa:2048 -keyout $CERTNAME -out $CERTNAME
+#using existing keys to sign pkgs
+if [ ! -f $SSLDIR/$PUBKEY ];
+then
+   echo "Public key NOT found. Generating certificate and public key in $SSLDIR directory..."
 
-openssl verify $CERTNAME
+   mkdir -p $SSLDIR
+
+   #generate both pub & priv keys in PEM format
+   openssl req -x509 -nodes -days 1800 \
+     -subj "/C=US/ST=Georgia/L=Atlanta/$O$CN$EA" \
+     -newkey rsa:2048 -keyout $SSLDIR/$CERTNAME -out $SSLDIR/$CERTNAME
+
+   openssl verify $SSLDIR/$CERTNAME
+
+   #extract public key from CERTNAME file
+   openssl x509 -in $SSLDIR/$CERTNAME -pubkey -outform PEM -out $SSLDIR/$PUBKEY
+else
+   echo "Public key found: $SSLDIR/$PUBKEY. Continuing package signing..."
+fi
 
 
-#extract public key from CERTNAME file                                                                                  
-openssl x509 -in $CERTNAME -pubkey -outform PEM -out $PUBKEY 
-
-gunzip $(find $1 -name Packages.gz)
-
-list=$(find $1 -name Packages)
-
-for i in $list
-do
-    openssl smime -sign -in $i -signer $CERTNAME -binary -outform PEM -out $i.sig
+for filename in $(find $1 -name Packages.gz); do
+    zcat $filename | openssl smime -sign -signer $SSLDIR/$CERTNAME -binary -outform PEM -out $(echo ${filename}| cut -d"." -f1).sig
 done
 
-gzip $(find $1 -name Packages)
 
-#extract public key from CERTNAME file
-openssl x509 -in $CERTNAME -pubkey -outform PEM -out $PUBKEY
-
-echo "** Please, update the $PUBKEY key in bismark-opkg-keys package. **"
+echo "** Please, update the $SSLDIR/$PUBKEY key in bismark-opkg-keys package. **"

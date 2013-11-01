819bf8dfa2dad478dda7ad3e84cf6eeeae84897f
ggmartins <ggmartins@gmail.com>
2013-11-01 17:33:31 -0400

    removed pubkey extration

--- scripts/signopkglist.sh
+++ scripts/signopkglist.sh
@@ -12,9 +12,6 @@ then
    echo "FOUND. Checking bismark-opkg-keys for server's pubkey ..."
    openssl verify $CERTPATH
 
-   #extract public key from CERTNAME file
-   openssl x509 -in $CERTPATH -pubkey -outform PEM -out $PUBKEY
-
    for filename in $(find $1 -name Packages.gz); do
       zcat $filename | openssl smime -sign -signer $CERTPATH -binary -outform PEM -out $(echo ${filename%.gz}.sig)
    done
